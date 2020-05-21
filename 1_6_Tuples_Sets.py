15fdf21d7e384dc97acffd3cfe35aaefb0ad1fa6
mehmet-can-8956 <65532954+mehmet-can-8956@users.noreply.github.com>
2020-05-21 04:39:24 +0300

    Add files via upload

---
+++ 1_6_Tuples_Sets.py
@@ -0,0 +1,5 @@
+week=("Monday","Tuesday","Wednesday","Thursday","Friday")
+fruits={"apple","mango","orange"}
+new_fruits={"cherry","peach","apple","mango"}
+print(new_fruits.difference(fruits))
+print(new_fruits.intersection(fruits))
