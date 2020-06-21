8d21d19d0483ea395cf032135fd557deefed1b70
mehmet-can-8956 <65532954+mehmet-can-8956@users.noreply.github.com>
2020-06-21 15:00:33 +0300

    Rename main.py to 1_1_Python_Basics.py

---
+++ 1_1_Python_Basics.py
@@ -0,0 +1,3 @@
+print("mehmet can")
+print("mehmet\ncan")
+print("""`I don't want to be an "artist". I want to be a "Data Scientist."`""")
--- main.py
+++
@@ -1,3 +0,0 @@
-print("mehmet can")
-print("mehmet\ncan")
-print("""`I don't want to be an "artist". I want to be a "Data Scientist."`""")
